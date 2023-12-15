db0f23967923a1d01b97df5b46af9f401342edfc
goran@rundberg.ax <goran@rundberg.ax>
2023-12-15 14:26:46 +0200

    yes

--- 01/011.go
+++ 01/011.go
@@ -5,9 +5,24 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
+
+	numMap := map[string]string{
+		"zero":  "zero0zero",
+		"one":   "one1one",
+		"two":   "two2two",
+		"three": "three3three",
+		"four":  "four4four",
+		"five":  "five5five",
+		"six":   "six6six",
+		"seven": "seven7seven",
+		"eight": "eight8eight",
+		"nine":  "nine9nine",
+	}
+
 	file, err := os.Open("input.txt")
 	if err != nil {
 
@@ -15,26 +30,28 @@ func main() {
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
-	scanner.Scan()
 	var num int
 	for scanner.Scan() {
-		var fnum, lnum rune
-		var first = true
-		for _, c := range scanner.Text() {
-			if c < '0' && c > '9' {
-				if first {
-					fnum = c
-				} else {
-					lnum = c
+		firstMatch := len(scanner.Text())
+		var fnum, lnum int
+		text := scanner.Text()
+		println("inp: ", text)
+		for k, v := range numMap {
+			text = strings.ReplaceAll(text, k, v)
+		}
+		println("out: ", text)
+
+		for p, c := range text {
+			if c >= '0' && c <= '9' {
+				if p < firstMatch {
+					fnum = int(c) - 48
+					firstMatch = p
 				}
+				lnum = int(c) - 48
 			}
 		}
-		var tmpnum int
-		tmpnum = int(fnum)
-		tmpnum = tmpnum*10+int(lnum)
-		num += tmpnum
+		num = num + fnum*10 + lnum
 	}
 
-	
 	fmt.Println(num)
 }
